8afd4717bb5ab26a7bc6bee72ea205fc3cd15aee
bojangles99 <afinebojangle@gmail.com>
2017-10-04 15:33:46 +0000

    made loop work even though it is ugly...

--- create_equity_historical_dataset.py
+++ create_equity_historical_dataset.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from datetime import timedelta, date
 
 
-engine = create_engine('postgresql://rayford:rhtpas0!@localhost:5432/market_data', echo=True)
+engine = create_engine('postgresql://bojangles:apppas0!@marketdata.chew6qxftqgr.us-east-1.rds.amazonaws.com:5432/marketdata', echo=True)
 
 Session = sessionmaker(bind=engine)
 
@@ -12,20 +12,29 @@ session = Session()
 
 quandl.ApiConfig.api_key = 'iasNNCL-zjwhdxME_Jvx'
 
-scraped_dates = session.query(EquityHistorical.date).distinct().all()
+scrapped_dates = []
+
+for value in session.query(EquityHistorical.date).distinct():
+    scrapped_dates.append(value.date)
 
 date_range = []
 
-for n in range((date.today() - date(2017, 3, 29)).days):
-        date_range.append(date(2017, 3, 29) + timedelta(n))
+for n in range((date.today() - date(2015, 3, 29)).days):
+        date_range.append(date(2015, 3, 29) + timedelta(n))
 
 call_count = 0
 
-while call_count < 50000:
-
-    for date in date_range:
-        if date.weekday() > 5:
-            pass
-
-        if date in scraped_dates:
-            pass
+while call_count < 2:
+    for day in date_range:
+        print(day)
+        if day.weekday() > 4:
+            print(day.weekday() > 4)
+        elif day in scrapped_dates:
+            print(day in scrapped_dates)
+        else:
+            data = quandl.get_table('WIKI/PRICES', date=day.strftime('%Y%m%d'))
+            data.rename(columns={'ex-dividend': 'ex_dividend'}, inplace=True)
+            data.to_sql('equity_historical', engine, if_exists='append', index=False, chunksize=5000)
+            scrapped_dates.append(day)
+            call_count += 1
+        
