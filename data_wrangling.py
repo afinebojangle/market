91ecb8b78d3a06991affae529ed98ca3aaac819e
Rayford <afinebojangle@gmail.com>
2018-02-21 16:29:07 -0500

    added drop duplicates to forward option prices

--- data_wrangling.py
+++ data_wrangling.py
@@ -240,8 +240,8 @@ class DataWrangling():
             data = pd.read_sql(query_string, db.engine)
             data['trade_date'] = day
             data.dropna(inplace=True)
-            column_order = [m.key for m in OptionForwardPrices.__table__.columns]
+            data.drop_duplicates(subset=['trade_date', 'ticker', 'strike', 'experiation_date'], keep=False, inplace=True)
+	    column_order = [m.key for m in OptionForwardPrices.__table__.columns]
             data = data[column_order]
             copy_dataframe_to_database(data, OptionForwardPrices, with_index=False)
-            
-    
+                
