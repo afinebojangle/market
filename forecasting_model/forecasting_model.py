9cbb77f5e03e1fe9d3913d831bb46704f51b73bb
Rayford <afinebojangle@gmail.com>
2018-04-19 16:29:55 -0400

    need to change dataset to produce "Positional" labels rather than string labels

--- forecasting_model/forecasting_model.py
+++ forecasting_model/forecasting_model.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
-
-tf.logging.set_verbosity(tf.logging.INFO)
+import shutil
 
 #whoops, pct_implied_volitility spelled wrong. Fixed in query but when new dataset is made will have to update spellings below.
 _CSV_COLUMNS = ['ticker', 'alpha', 'beta', 'trade_type', 'years_to_experiation', 'pct_capm_error',
@@ -18,8 +17,7 @@ LABEL_VOCAB = ['-51', '-52', '-53', '-54', '-55', '-56', '-57',
                '41', '42', '43', '44', '45', '46', '47',
                '51', '52', '53', '54', '55', '56', '57']
 
-_LABEL_DEFAULT_TENSOR = tf.constant(0, dtype=tf.float32, shape=(len(LABEL_VOCAB),))
-
+#last default for label has to be an int data type since after we convert labels to one-hots below the one-hot will hold ints and tensorflow cares.
 _CSV_COLUMN_DEFAULTS = [[''], [0.0], [0.0], [''], [0.0], [0.0],
                         [0.0], [0.0], [0.0], [0.0], [0.0],
                         [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0]]
@@ -44,6 +42,9 @@ def build_feature_columns():
     phi = tf.feature_column.numeric_column('phi', dtype=tf.float32)
 
     feature_columns = [
+                tf.feature_column.indicator_column(ticker),
+                alpha,
+                beta,
                 tf.feature_column.indicator_column(trade_type),
                 years_to_experiation,
                 pct_capm_error,
@@ -68,13 +69,18 @@ def input_fn(data_file, num_epochs, shuffle, batch_size):
         print('Parsing', data_file)
         columns = tf.decode_csv(value, record_defaults=_CSV_COLUMN_DEFAULTS)
         features = dict(zip(_CSV_COLUMNS, columns))
-        labels = tf.one_hot(features.pop('label'), len(LABEL_VOCAB), on_value=1, off_value=0)
-        return {'features': features, 'labels': labels}
+        try:
+            labels = tf.one_hot(LABEL_VOCAB.index(features.pop('label').eval()), len(LABEL_VOCAB))
+        except:
+            print('-' * 60)
+            print('Error Encoding Label')
+            labels = tf.one_hot(1, len(LABEL_VOCAB))
+
+        return features, labels
 
     dataset = tf.data.TextLineDataset(data_file).skip(1)
 
     if shuffle:
-        #dataset = dataset.shuffle(buffer_size=_NUM_EXAMPLES['train'])
         dataset = dataset.shuffle(buffer_size=100000000)
 
     dataset = dataset.map(parse_csv, num_parallel_calls=6)
@@ -82,9 +88,9 @@ def input_fn(data_file, num_epochs, shuffle, batch_size):
     dataset = dataset.batch(batch_size)
     return dataset.make_one_shot_iterator().get_next()
 
-def model_fn(features, feature_columns, labels, mode):
+def model_fn(features, labels, mode, params):
     #input layer
-    input_layer = tf.feature_column.input_layer(features, feature_columns)
+    input_layer = tf.feature_column.input_layer(features, params['feature_columns'])
 
     #hidden layers
     hidden1 = tf.layers.dense(inputs=input_layer, units=128, activation=tf.nn.relu)
@@ -92,21 +98,60 @@ def model_fn(features, feature_columns, labels, mode):
     hidden3 = tf.layers.dense(inputs=hidden2, units=128, activation=tf.nn.relu)
 
     #logits
-    logits = tf.layer.dense(inputs=hidden3, units=len(LABEL_VOCAB), activation=None)
+    logits = tf.layers.dense(inputs=hidden3, units=len(LABEL_VOCAB), activation=None)
 
     #compute predictions
+    predicted_classes = tf.argmax(logits, 1)
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        predictions = {
+            'class_ids': predicted_classes[:, tf.newaxis],
+            'probabilities': tf.nn.softmax(logits),
+            'logits': logits,
+        }
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+    #compute loss
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=logits)
+
+    #compute evaluation metrics
+    accuracy = tf.metrics.accuracy(labels=labels, predictions=predicted_classes, name='acc_op')
+
+    metrics = {'accuracy': accuracy}
+    tf.summary.scalar('accuracy', accuracy)
+
+    optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
+    train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+
+    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op_)
+
+def main(argv):
+    # Clean up the model directory if present
+    shutil.rmtree('/home/rayford/code/market/forecasting_model/run1', ignore_errors=True)
+
+    feature_columns = build_feature_columns()
+
+    def train_input_fn():
+        return input_fn('/home/rayford/code/tensorflow_data/training_data.csv', 1, True, 1000)
+
+    def eval_input_fn():
+        return input_fn('home/rayford/code/tensorflow_data/testing_data.csv', 1, False, 1000)
+
+    classifier = tf.estimator.Estimator(
+        model_fn=model_fn,
+        model_dir='/home/rayford/code/market/forecasting_model/run1',
+        params={
+            'feature_columns': feature_columns,
+        })
+
+    #train the model
+    classifier.train(input_fn=train_input_fn, steps=160000)
+
+    #evaluate the model
+    eval_result = classifier.evaluate(input_fn=eval_input_fn)
+
+    print('\nTest Set Accuracy: {accuracy:0.3f}\n'.format(**eval_result))
 
 
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-  tf.app.run()
+if __name__ == '__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run()
